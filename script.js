f343a56508b2afe0910e70c8a9e223599a5ef48c
Mari Azh <Marzi.aghchay@gmail.com>
2023-02-01 22:18:25 +0000

    the episode's name

--- script.js
+++ script.js
@@ -8,6 +8,12 @@ function setup() {
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  //the episode's name
+  episodeList.forEach(episode => {
+    const p =document.createElement("p");
+    p.textContent = `episode name : ${episode.name}`
+    rootElem.appendChild(p);
+  });
 }
 
 window.onload = setup;
