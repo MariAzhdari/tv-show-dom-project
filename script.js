b85fdfb7c5f59c34b5b87802378b8748f81c1fdc
Mari Azh <Marzi.aghchay@gmail.com>
2023-02-02 10:43:28 +0000

    challenge number 3 in syllabus

--- script.js
+++ script.js
@@ -5,13 +5,25 @@ function setup() {
   makePageForEpisodes(allEpisodes);
 }
 
+const makeSeasonAndEpisodes = function(episode){
+//shot form for const season and const number 
+const{season,number} = episode;
+// const season =episode.season ;
+// const number =episode.number ;
+const paddedSeason = season.toString().padStart(2,"0");
+
+const paddedEpisodeNumber = number.toString().padStart(2,"0");
+
+return `S${paddedSeason}E${paddedEpisodeNumber}`
+}
+
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
   //the episode's name and season number and episode number
   episodeList.forEach(episode => {
     const p = document.createElement("p");
-    p.textContent = `S${episode.season}E${episode.number}-name: ${episode.name}`
+    p.textContent = `${makeSeasonAndEpisodes(episode)}-name: ${episode.name}`
     rootElem.appendChild(p);
     //the episode's medium-sized image
     const img = document.createElement("img");
